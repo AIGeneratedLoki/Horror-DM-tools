7ab0799e6d9acf7ee281d85a6fc61c4cfdc797c2
Rick Rice <rickrice2nd@gmail.com>
2025-09-10 14:33:59 -0700

    style work again

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,8 @@
-
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/Horror-DM-tools',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https' as const,
-        hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  devIndicators: {
-    allowedDevOrigins: [
-      '*.cloudworkstations.dev',
-    ]
-  },
+  /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
