84dc834c3c78d1fd2f12fa2ede2853930d7b122e
Rick Rice <rickrice2nd@gmail.com>
2025-09-10 11:22:56 -0700

    fix comma in next.congig.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Horror-DM-tools'
+  basePath: '/Horror-DM-tools',
   typescript: {
     ignoreBuildErrors: true,
   },
